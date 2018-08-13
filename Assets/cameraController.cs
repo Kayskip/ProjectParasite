637e1f7dc647ed83eac30f5dc18ef944125fce0f
porterjack <porterjack@ecs.vuw.ac.nz>
2018-08-13 22:39:48 +1200

    added player movement and collision with map objects

---
+++ Assets/cameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    public GameObject Player;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        transform.position = Player.transform.position + offset;
+             
+	}
+}
