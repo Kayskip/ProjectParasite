2cb7934bba625cbed78bc3f3353c7804e205dbe7
porterjack <porterjack@ecs.vuw.ac.nz>
2018-08-14 12:07:36 +1200

    player points at camera

--- Assets/followMouse.cs
+++ Assets/followMouse.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class followMouse : MonoBehaviour {
-
+    private Camera cam;
 	// Use this for initialization
 	void Start () {
-		
+        cam = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 mousepos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 perpendicular = Vector3.Cross(transform.position - mousepos, Vector3.forward);
+        transform.rotation = Quaternion.LookRotation(transform.position - mousepos, Vector3.forward);
 	}
 }
