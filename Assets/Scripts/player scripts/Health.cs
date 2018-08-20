4bd1df376a05f2895b76c51506c7fa112ef60d10
porterjack <porterjack@ecs.vuw.ac.nz>
2018-08-20 19:11:52 +1200

    added health script and basic shooting

---
+++ Assets/Scripts/player scripts/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int maxHealth;
+    private int currentHealth;
+    private bool isHit;
+	// Use this for initialization
+	void Start () {
+        isHit = false;
+        currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isHit)
+        {
+            currentHealth--;
+        }
+        if (currentHealth == 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
