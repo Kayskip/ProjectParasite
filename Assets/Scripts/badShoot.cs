23529148f04cbfe74c5c2f1c0fd227d0c0aa0784
porterjack <porterjack@ecs.vuw.ac.nz>
2018-08-21 01:26:27 +1200

    added working doors and keys, turret now aims and shoots automatically

---
+++ Assets/Scripts/badShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class badShoot : MonoBehaviour
+{
+    float interval = 1;
+
+    public Transform firepoint;
+    public GameObject bullet;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        interval -= Time.deltaTime;
+        if (interval < 0)
+        {
+            shoot();
+            interval = 1;
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(bullet, firepoint.position, firepoint.rotation);
+    }
+}
---
+++ Assets/Scripts/followPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour {
+    Vector3 playerpos;
+    GameObject player;
+    // Use this for initialization
+    void Start () {
+         player = GameObject.Find("Player 1");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Transform playerTransform = player.gameObject.GetComponent<Transform>();
+        playerpos = playerTransform.position;
+        Quaternion rotation = Quaternion.LookRotation(transform.position - playerpos, Vector3.forward);
+        rotation.x = 0;
+        rotation.y = 0;
+        transform.rotation = rotation;
+
+    }
+}
